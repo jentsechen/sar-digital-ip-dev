cc39032e49b2464649e0f7233b3481961340deeb
jentsechen <jentsechen1009@gmail.com>
2026-06-30 19:08:49 +0800

    Add design

---
+++ source_file/design/CmultImplGeneric/firrtl_black_box_resource_files.f
@@ -0,0 +1,4 @@
+/workspaces/chisel3digdev/vivado/SAR/Rx/SarRxDspNewBeamformOneClock/source/debug/XBMEM.v
+/workspaces/chisel3digdev/vivado/SAR/Rx/SarRxDspNewBeamformOneClock/source/debug/SDPDistrMem.v
+/workspaces/chisel3digdev/vivado/SAR/Rx/SarRxDspNewBeamformOneClock/source/debug/SimpleDualPortMem.v
+/workspaces/chisel3digdev/vivado/SAR/Rx/SarRxDspNewBeamformOneClock/source/debug/SRL.v
